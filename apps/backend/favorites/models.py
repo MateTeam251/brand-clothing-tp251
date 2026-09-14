547fe71745a0b9e79f27262b7120cad933b3a238
Illia Savinov <dresar2404@gmail.com>
2026-09-14 14:25:33 +0200

    feat: Add favorites model

--- apps/backend/favorites/models.py
+++ apps/backend/favorites/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="favorites") #only for registered users
+    product = models.ForeignKey(
+        "products.Product", on_delete=models.CASCADE, related_name="favorited_by")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["product", "user"],
+                name="unique_product_per_user"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} - {self.product.name}"
