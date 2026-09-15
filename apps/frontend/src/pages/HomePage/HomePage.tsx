0f9e77b567fd0eb80a3c8f90079e2e42c4ea4a49
Skoulskiy <yaskevychilya@gmail.com>
2026-09-15 12:13:48 +0300

    feature: added userSlice, api file and new hook useLocalStorage

--- apps/frontend/src/pages/HomePage/HomePage.tsx
+++ apps/frontend/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
+import { fetchUser } from "../../app/store/reducers/userSlice";
+import { useAppDispatch, useAppSelector } from "../../shared/hooks/reduxHooks";
+import { useLocalStorage } from "../../shared/hooks/useLocalStorage";
 
 
 export const HomePage = () => {
+  const dispatch = useAppDispatch();
+  const [accessToken] = useLocalStorage<string | null>("accessToken", null);
+  
+  const user = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    if (accessToken && !user.id) {
+      dispatch(fetchUser(accessToken));
+    }
+  }, [accessToken, user.id, dispatch]);
+
   return (
     <div>
       <h1>Home Page</h1>
--- apps/frontend/src/shared/api/api.ts
+++ apps/frontend/src/shared/api/api.ts
@@ -1,1 +1,36 @@
-// for api
+import type { User } from "../types/User";
+
+const API_BASE_URL = 'http://127.0.0.1:8000/api';
+
+export type TokenResponse = {
+  access: string;
+  refresh: string;
+}
+
+async function request<T>(url: string, options?: RequestInit): Promise<T> {
+  const response = await fetch(`${API_BASE_URL}${url}`, options);
+
+  if(!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const getUser = async (accessToken: string) => {
+  return request<User>(`/users/me/`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    }
+  });
+}
+
+export const loginUser = async (email: string, password: string) => {
+  return request<TokenResponse>(`/users/token/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+};
