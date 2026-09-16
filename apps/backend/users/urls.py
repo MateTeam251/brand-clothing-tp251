2959f1d85ce596614f42aa11663b20cb6e60304c
Illia Savinov <dresar2404@gmail.com>
2026-09-16 12:35:46 +0200

    chore: changed login/ endpoint to custom one

--- apps/backend/users/urls.py
+++ apps/backend/users/urls.py
@@ -4,13 +4,12 @@ from users.views import (RegisterView,
                          MeView,
                          LogoutView,
                          PasswordResetRequestView,
-                         PasswordResetConfirmView, AddressView)
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+                         PasswordResetConfirmView, AddressView, LoginView)
+from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("activate/<str:uidb64>/<str:token>/", ActivateAccountView.as_view(), name="activate"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("password-reset/", PasswordResetRequestView.as_view(), name="password_reset_request"),
