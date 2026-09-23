35969cd89b374ee3205125030813c7a0239f6c44
maxmlv <max.mlv.k@gmail.com>
2026-09-23 21:17:57 +0300

    fix: region + tfstate bucket name

--- infra/environments/prod/backend.tf
+++ infra/environments/prod/backend.tf
@@ -14,7 +14,7 @@ terraform {
 
   backend "s3" {
     # Fill these in from the outputs of infra/bootstrap.
-    bucket         = "REPLACE_WITH_STATE_BUCKET_NAME"
+    bucket         = "brand-clothing-tfstate-971441629275-eu"
     key            = "prod/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "brand-clothing-terraform-lock"
